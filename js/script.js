4cb43d8b39ade27c7e64a543800fbd79a5b6493c
daryananidude <shivdaryanani@me.com>
2016-03-13 18:36:34 +0800

    fixes and pictures input

--- js/script.js
+++ js/script.js
@@ -115,6 +115,29 @@ if(!result){
 }
 document.write('<meta name="viewport" content="width=device-width,initial-scale=1.0'+userScale+'">')
 
+<script>
+window.twttr = (function (d,s,id) {
+
+    var t, js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return; js=d.createElement(s); js.id=id;
+    js.src="https://platform.twitter.com/widgets.js";
+    fjs.parentNode.insertBefore(js, fjs);
+
+    return window.twttr || (t = { _e: [], ready: function(f){ t._e.push(f) } });
+
+}(document, "script", "twitter-wjs"));
+
+twttr.ready(function (twttr) {
+
+    twttr.widgets.load();
+    setInterval(function() {
+        twttr.widgets.load();
+        console.log("update twitter timeline each second");
+    }, 1000);
+
+});
+</script>
+
 /* custom hover
 ========================================================*/
 
