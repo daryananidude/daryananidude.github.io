2ee0793c6e87f9ebe4a17cf74257006756eff880
daryananidude <shivdaryanani@me.com>
2018-05-17 09:33:16 +0800

    Fitness tracker

---
+++ jstest/script.js
@@ -0,0 +1,6 @@
+numero = prompt("Enter numero from 1 to infinite_vegetal")
+var numero = parseInt(numero)
+
+for (i=0; i<13; i++){
+	document.write(numero + " x " + i + " = " + (numero*i)+"<br>")
+}
